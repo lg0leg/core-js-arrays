37f5ac7ac6eab70a690bfa4be2a6c9b50d3820ae
lg0leg <lg.oleg1067@gmail.com>
2024-01-05 23:34:52 +0300

    feat: add sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,12 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const l = arr1.length > arr2.length ? arr1.length : arr2.length;
+  return Array.from(
+    { length: l },
+    (_, index) => (arr1.at(index) || 0) + (arr2.at(index) || 0)
+  );
 }
 
 /**
